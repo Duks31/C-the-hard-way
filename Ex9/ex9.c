4f143db61a9cc9c453503fd3da4b19448fe77718
Duks31 <chidubemjan31@gmail.com>
2024-09-28 17:48:42 +0100

    ex9

---
+++ Ex9/ex9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// int main(int argc, char* argv[])
+// {
+//     int i = 0;
+//     while (i < 25) {
+//         printf("%d ", i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int i = 25;
+//     while (i > 0){
+//         printf("%d ", i);
+//         i--;
+//     }
+// }
+
