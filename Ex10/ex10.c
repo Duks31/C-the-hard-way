3214bbad0b287d62cdadd848ed9bd28b7d942efa
Duks31 <chidubemjan31@gmail.com>
2024-09-28 18:42:21 +0100

    ex10

---
+++ Ex10/ex10.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+// int main(int argc, char* argv[])
+// {
+//     int i = 0;
+
+//     for (i = 1; i < argc; i++) {
+//         printf("arg %d: %s\n", i, argv[i]);
+//     }
+
+//     char *states[] = {
+//         "California", "Oregon",
+//         "Washington", " Texas"
+//     };
+
+//     int num_states = 4;
+
+//     for (i = 0; i < num_states; i++) {
+//         printf("state %d: %s\n", i, states[i]);
+//     }
+
+//     return 0;
+// }
+
+int main(int argc, char* argv[]) 
+{
+    int var = 1;
+
+    switch (var )
+    {
+    case 1:
+        printf("Case 1 Matched");
+        break;
+
+    case 2:
+        printf("Case 2 Matched");
+        break;
+    
+    default:
+        printf("I guess i would have to go here, Nothing Matched!!!");
+        break;
+    }
+}
